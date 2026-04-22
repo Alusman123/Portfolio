780ebfb11ededa1c430dc94c30fabe80976efc3f
Alusman123 <alyas3933@gmail.com>
2026-04-22 11:08:22 +0800

    First Update

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,25 @@
 import './App.css'
+import { BriefInfoScreen } from './Screen/BriefInfoScreen'
+import { Mainpage } from './Screen/MainPage';
+import TopoBg from './assets/topgif.mp4';
 
 function App() {
-
-
   return (
-   <div className="flex flex-row bg-amber-300">
-    <div className="px-1">
-      sup
+    <div className="relative min-h-screen w-full flex items-center justify-center">
+      <video
+        autoPlay
+        loop
+        muted
+        playsInline
+        className="absolute inset-0 w-full h-full object-cover"
+      >
+        <source src={TopoBg} type="video/mp4" />
+      </video>
+
+  <Mainpage/>
+  
     </div>
-    <div>hello</div>
-   </div>
+    
   )
 }
 
---
+++ src/Screen/Resume.tsx
@@ -0,0 +1,12 @@
+export function Resume(){
+return(
+   <div 
+   className="h-[700px] w-[1300px] rounded-[20px] px-10 py-10 flex flex-col gap-5 overflow-y-auto">
+ Resume
+    <p>3</p>
+   
+
+</div>
+);
+}
+
